ae5f93a6a6a629afaa28a746eb9a824f7239ef9f
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-02-20 20:17:26 -0300

    Adiciona boilerplate html a index.html

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from pathlib import Path
 
+def insert_boilerplate(directory):
+    with open('boilerplate_html.txt', 'r') as origin:
+        with open(f'{directory}/index.html', 'a') as destiny:
+            for line in origin:
+                destiny.write(line)
+
 def create_project():
-    path = Path('.')
+    path = Path('teste')
 
     (path / 'js').mkdir(parents=True, exist_ok=True)
     (path / 'assets').mkdir(parents=True, exist_ok=True)
@@ -10,5 +16,6 @@ def create_project():
 
     arquivo = (path / 'index.html')
     arquivo.touch()
+    insert_boilerplate(path)
 
 create_project()
