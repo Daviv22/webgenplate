4fe2592460fa23de0cae9f3884ae400e8a896b54
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-02-22 14:54:20 -0300

    Adiciona tipos de template

--- cli.py
+++ cli.py
@@ -1,10 +1,23 @@
 import argparse
 from generator import  create_project
 
-parser = argparse.ArgumentParser(description="Script para automatização de projetos")
+parser = argparse.ArgumentParser(
+    description="Script para automatização de projetos"
+)
 
-parser.add_argument('directory_name', type=str, help='Nome do diretório onde o projeto será criado')
+parser.add_argument(
+    'directory_name',
+    type=str,
+    help='Nome do diretório onde o projeto será criado'
+)
 
+parser.add_argument(
+    '--template',
+    type=str,
+    choices=['basic'],
+    default='basic',
+    help='Template do projeto'
+)
 args = parser.parse_args()
 
-create_project(args.directory_name)
+create_project(args.directory_name, args.template)
