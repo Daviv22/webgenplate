406646d36caa78856b8d3ccdd66bd50552f003e8
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-02-22 15:32:53 -0300

    Coloca código cli.py dentro da função main()

--- cli.py
+++ cli.py
@@ -1,23 +1,24 @@
 import argparse
 from generator import  create_project
 
-parser = argparse.ArgumentParser(
-    description="Script para automatização de projetos"
-)
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script para automatização de projetos"
+    )
 
-parser.add_argument(
-    'directory_name',
-    type=str,
-    help='Nome do diretório onde o projeto será criado'
-)
+    parser.add_argument(
+        'directory_name',
+        type=str,
+        help='Nome do diretório onde o projeto será criado'
+    )
 
-parser.add_argument(
-    '--template',
-    type=str,
-    choices=['basic'],
-    default='basic',
-    help='Template do projeto'
-)
-args = parser.parse_args()
+    parser.add_argument(
+        '--template',
+        type=str,
+        choices=['basic'],
+        default='basic',
+        help='Template do projeto'
+    )
+    args = parser.parse_args()
 
-create_project(args.directory_name, args.template)
+    create_project(args.directory_name, args.template)
