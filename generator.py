8ba9d6540ec7bc2428edfaceb7d59e06bacf1177
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-02-22 14:53:46 -0300

    Adiciona tipos de template

--- generator.py
+++ generator.py
@@ -2,17 +2,28 @@ from pathlib import Path
 
 TEMPLATE_HTML_FILE = Path('boilerplates/html_boilerplate.txt')
 
+def basic_project(base):
+    for folder in ["js", "css", "assets"]:
+        (base / folder).mkdir(parents=True, exist_ok=True)
+
+    insert_boilerplate(base)
+
+COMMANDS = {
+    "basic": basic_project
+}
 def insert_boilerplate(directory):
     destiny = directory / 'index.html'
     destiny.write_text(TEMPLATE_HTML_FILE.read_text())
 
-def create_project(directory):
+def create_project(directory, template):
     base = Path(directory)
 
-    for folder in ["js", "css", "assets"]:
-        (base / folder).mkdir(parents=True, exist_ok=True)
+    template_type = COMMANDS.get(template)
+
+    if template_type:
+        template_type(base)
+    else:
+        return
 
     (base / 'README.md').touch()
-
-    insert_boilerplate(base)
 
