543ed601498901303e92f0048f1319cd100b95ba
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-02-22 13:37:07 -0300

    Corrige erros

--- generator.py
+++ generator.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
-TEMPLATE_HTML_FILE = Path('boilerplates/boilerplate_html.txt')
+TEMPLATE_HTML_FILE = Path('boilerplates/html_boilerplate.txt')
 
 def insert_boilerplate(directory):
     destiny = directory / 'index.html'
     destiny.write_text(TEMPLATE_HTML_FILE.read_text())
 
-def create_project():
-    base = Path('teste')
+def create_project(directory):
+    base = Path(directory)
 
     for folder in ["js", "css", "assets"]:
         (base / folder).mkdir(parents=True, exist_ok=True)
@@ -16,4 +16,3 @@ def create_project():
 
     insert_boilerplate(base)
 
-create_project()
